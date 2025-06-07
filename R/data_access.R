e3eb4901714952621fa1ed34d839e69ac6d5ee26
lmalmud <lmalmud2@jh.edu>
2025-06-07 14:49:09 -0400

    fixing data_access

--- R/data_access.R
+++ R/data_access.R
@@ -1,6 +1,22 @@
 # data_access.R
 # Functions to download data from SQL data for a particular ticker.
-library(RPostgreSQL)
+
+library(RPostgres) # For creating the driver
+library(glue) # For safe connections (https://glue.tidyverse.org/reference/glue_sql.html#:~:text=glue_sql.Rd,DBI%3A%3ASQL()%20objects.)
+
+get_db_connection <- function() {
+  dbConnect(RPostgres::Postgres(), # Driver
+            dbname = "factor_data",
+            host = "db",
+            port = 5432,
+            user = "postgres",
+            password = "postgres");
+}
+
+get_returns_data <- function(conn, ticker) {
+  query <- glue::glue_sql("SELECT * FROM vw_returns WHERE ticker = {ticker}", .con = conn)
+  df <- dbGetQuery(conn, query)
+}
 
 dbGetQuery <- function(ticker) {
   drv <- dbDriver("PostgreSQL") # Need a driver that is PostgreSQL
@@ -16,7 +32,3 @@ dbGetQuery <- function(ticker) {
 }
 
 dbGetQuery('MSFT')
-
-# to ensure that bind-mount working: docker-compose exec r-model ls /app/R
-# to build: docker compose build
-# docker compose run r-model
